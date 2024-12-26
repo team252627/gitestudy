3b102bfc92422fcdd9b82b6fbf69f395d16098fb
mwagmwag <kinggusi50@gmail.com>
2024-12-27 00:57:47 +0900

    dev 변경 테스트 1

--- backend/src/main/java/com/example/backend/BackendApplication.java
+++ backend/src/main/java/com/example/backend/BackendApplication.java
@@ -1,14 +1,14 @@
 package com.example.backend;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class BackendApplication {
-
 	public static void main(String[] args) {
-
+		log.debug("파일 변경 커밋 확인 ▶▶▶▶▶▶▶▶▶");
 		SpringApplication.run(BackendApplication.class, args);
-
 	}
 }
